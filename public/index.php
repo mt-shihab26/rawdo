0cde09d791f9cad5666bf38fb536171f60cfbccd
Shihab Mahamud <shihab4t@gmail.com>
2026-08-31 21:52:16 +0600

    Tidy gitignore and drop static-file bypass in the front controller

--- public/index.php
+++ public/index.php
@@ -2,16 +2,6 @@
 
 use Src\Core\App;
 
-// Let the built-in dev server serve static files directly instead of
-// routing them through the app
-if (PHP_SAPI === 'cli-server') {
-    $file = __DIR__.parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-    if (is_file($file)) {
-        return false;
-    }
-}
-
 require __DIR__.'/../vendor/autoload.php';
 
 $app = new App;
